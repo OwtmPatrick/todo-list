8ac01922d6486f49d2c149bd912693985519ac1a
Radik Zangirov <owtmpatrick@gmail.com>
2023-09-27 09:18:08 +0500

    add wallet connection

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
+import Example from './example/Example';
+import { Web3ContextProvider } from './libs/components/Web3ContextProvider';
+
+if (window.ethereum) {
+  window.ethereum.autoRefreshOnNetworkChange = false;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Web3ContextProvider>
+      <Example />
+    </Web3ContextProvider>
   </React.StrictMode>
 );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface Window {
+  ethereum?: {
+    // value that is populated and returns true by the Coinbase Wallet mobile dapp browser
+    isCoinbaseWallet?: true;
+    isMetaMask?: true;
+    autoRefreshOnNetworkChange?: boolean;
+    isBraveWallet?: true;
+  };
+}
