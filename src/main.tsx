02c3f8dd2444dc0eefa6a29e165c30a6cfcfb2c6
Radik Zangirov <owtmpatrick@gmail.com>
2023-09-27 15:14:12 +0500

    add buffer

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Buffer } from 'buffer';
 import Example from './example/Example';
 import { Web3ContextProvider } from './libs/components/Web3ContextProvider';
 
 if (window.ethereum) {
   window.ethereum.autoRefreshOnNetworkChange = false;
 }
+
+// Node polyfills required by WalletConnect
+window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
