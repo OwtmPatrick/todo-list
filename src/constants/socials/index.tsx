fdac1303b3074a59b5e2f58bc0a423ea445b75e6
Radik Zangirov <owtmpatrick@gmail.com>
2023-10-01 15:19:42 +0500

    fix type

--- src/constants/socials/index.tsx
+++ src/constants/socials/index.tsx
@@ -1,8 +1,8 @@
 import type { Social } from '../../types';
 
 export const socials: Social[] = [
-  { href: 'https://facebook.com', alt: 'facebook', icon: 'facebook' },
-  { href: 'https://twitter.com', alt: 'twitter', icon: 'twitter' },
-  { href: 'https://youtube.com', alt: 'youtube', icon: 'youtube' },
-  { href: 'https://instagram.com', alt: 'instagram', icon: 'instagram' }
+  { href: 'https://facebook.com', icon: 'facebook' },
+  { href: 'https://twitter.com', icon: 'twitter' },
+  { href: 'https://youtube.com', icon: 'youtube' },
+  { href: 'https://instagram.com', icon: 'instagram' }
 ];
